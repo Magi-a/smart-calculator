fda65ac4737e28cea3456805fbad14e51ee9a972
Nik <nik-pavlovich@mail.ru>
2018-10-04 00:18:27 +0300

    306p/306

--- src/index.js
+++ src/index.js
@@ -1,26 +1,85 @@
 class SmartCalculator {
+  
   constructor(initialValue) {
-    // your implementation
+    this.expression = [];
+    this.expression.push(initialValue);
+    
   }
 
   add(number) {
-    // your implementation
+    this.expression.push('+');
+    this.expression.push(number);
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.expression.push('-');
+    this.expression.push(number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.expression.push('*');
+    this.expression.push(number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.expression.push('/');
+    this.expression.push(number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.expression.push('^');
+    this.expression.push(number);
+    return this;
+  }
+
+  valueOf() {
+
+    // '*' and '/'
+
+    for(let i = this.expression.length-1; i >= 0; i--){
+      if(this.expression[i] === '^'){
+        console.log(this.expression);
+        var temp = Math.pow(this.expression[i-1], this.expression[i+1]);
+        this.expression.splice([i-1], 3 , temp);
+      }
+    }
+
+    while(this.expression.indexOf('*')!== -1){
+      console.log(this.expression);
+      var i = this.expression.indexOf('*');
+      var temp = this.expression[i-1] * this.expression[i+1];
+      this.expression.splice([i-1], 3 , temp);
+    }
+    
+    while(this.expression.indexOf('/')!== -1){
+      console.log(this.expression);
+      var i = this.expression.indexOf('/');
+      var temp = Math.floor(this.expression[i-1] / this.expression[i+1]);
+      this.expression.splice([i-1], 3 , temp);
+    }
+
+    while(this.expression.indexOf('-')!== -1){
+      console.log(this.expression);
+      var i = this.expression.indexOf('-');
+      var temp = this.expression[i-1] - this.expression[i+1];
+      this.expression.splice([i-1], 3 , temp);
+    }
+
+    while(this.expression.indexOf('+')!== -1){
+      console.log(this.expression);
+      var i = this.expression.indexOf('+');
+      var temp = this.expression[i-1] + this.expression[i+1];
+      this.expression.splice([i-1], 3 , temp);
+    }
+
+
+    console.log(this.expression);
+
+    return this.expression[0];
   }
 }
 
